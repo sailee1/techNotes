cfc92833265afe5aaf2700e027eb0f10ad02e65b
sailee1 <saiclee1986@googlemail.com>
2022-08-17 10:47:50 +0100

    Create and viewing notes within frontend

--- models/Note.js
+++ models/Note.js
@@ -1,35 +1,34 @@
 const mongoose = require('mongoose')
 const AutoIncrement = require('mongoose-sequence')(mongoose)
 
-const noteSchema = mongoose.Schema(
+const noteSchema = new mongoose.Schema(
     {
-    user:{
-        type: mongoose.Schema.Types.ObjectId, 
-        required: true,
-        ref: 'User'
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
     },
-    title:{
-        type: String, 
-        required: true
-    },
-    text:{
-        type: String, 
-        required: true
-    },
-    completed:{
-        type: Boolean,
-        default: false
+    {
+        timestamps: true
     }
-}, 
-{
-    timeStamps: true
-}
 )
-
 
 noteSchema.plugin(AutoIncrement, {
     inc_field: 'ticket',
-    id:'ticketNums',
+    id: 'ticketNums',
     start_seq: 500
 })
 
